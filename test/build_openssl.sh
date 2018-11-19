76ee653b1baee23a09937422d0418d37726837ac
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-18 23:13:30 -0600

    Use openssl 1.1.1

---
+++ test/build_openssl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+die() {
+	echo >&2 "$@"
+	exit 0
+}
+
+[[ -d "${OPENSSL_DIR}/lib" ]] && die "Directory already exists, refusing to build again"
+rm -r ${OPENSSL_DIR}
+mkdir /tmp/build/
+[[ -d /tmp/build ]] || die "Failed to create build directory"
+cd /tmp/build/
+curl https://www.openssl.org/source/openssl-1.1.1.tar.gz -o /tmp/build/openssl.tar.gz 
+[[ -f /tmp/build/openssl.tar.gz ]] || die "Failed to download openssl source code"
+tar xzf /tmp/build/openssl.tar.gz && cd openssl-1.1.1
+./Configure --prefix=${OPENSSL_DIR} --openssldir=${OPENSSL_DIR} linux-x86_64 -fPIC -g no-shared
+make -j$(nproc)
+make install
+rm -r /tmp/build/
+cd ~/build/sameer/lrad
